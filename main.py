450ccaa5cbf533a49d64650c51112b90c778258e
koany <koroliev@yandex.ru>
2023-06-24 15:24:59 +0300

    Готово в принципе

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ print('\n\n***************Задача №3***************')
 from random import randint as rand
 
 marks = ['audi', 'bmw', 'lada', 'tesla', 'toyota']
-numbers = ['qqq111', 'www222', 'eee333', 'rrr444', 'ttt555']
+numbers = ['к384ан', 'у745ом', 'л857нв', 'в548оп', 'г754св']
 people = ['Ivan', 'John', 'Antonio', 'Hideo', 'Hans']
 owners = []
 
@@ -95,45 +95,67 @@ for i in range(len(numbers)):
 registry = list(zip(marks, numbers, owners))
 
 
-# вывод ассортимента авто со списком владельцев
-def print_cars():
-    print('Вот что есть в наличии:')
-    for i in registry:
-        print('\tМарка:', i[0], '\n\t', 'Регистрационный номер:', i[1], '\n\t', 'Список владельцев:', ', '.join(i[2]))
-
 
 # вывод одного авто
-def print_car():
-    print('Информация о выбранном авто:')
+def print_car(i):
+    print('\tМарка: ', i[0], '\n\t\tРегистрационный номер: ', i[1], '\n\t\tСписок владельцев: ', ', '.join(i[2]), sep='')
 
 
 # найти авто по номеру
-def find_by_number(num):
-    pass
+def find_by_number(num, reg):
+    for i in reg:
+        if num in i:
+            return reg.index(i)
+    return -1
 
-# def buy_car():
-#     ('Введите регистрационный номер авто, который хотите купить:')
+
 
 print('Добро пожаловать в наш автосалон')
 while True:
     try:
-        ans = int(input('Что вас интересует?\n'
+        ans = int(input('\nЧто вас интересует?\n'
                   '\t1 - посмотреть ассортимент авто\n'
                   '\t2 - купить\n'
                   '\t3 - продать\n'
                   '\t4 - найти по номеру\n'
                   '\t0 - покинуть салон\n'))
         match ans:
-            case 0:
+            case 0:  # покинуть салон
+                print('Ждём вас снова. Всего доброго!')
                 break
-            case 1:
-                print_cars()
-            case 2:
-                pass
-            case 3:
-                pass
-            case 4:
-                pass
+            case 1:  # вывод всех авто
+                print('Вот что есть в наличии:')
+                for i in registry:
+                    print_car(i)
+            case 2:  # покупка авто
+                num = input('Введите регистрационный номер авто, который хотите купить:\n')
+                num_ind = find_by_number(num, registry)
+                if num_ind >= 0:
+                    print('Информация о выбранном авто:')
+                    print_car(registry[num_ind])
+                    ans = input('Подтверждаете покупку? (для подтверждения введите \'д\', любой другой символ для отказа)\n')
+                    if ans.lower() == 'д':
+                        registry.pop(num_ind)
+                        print('Поздравляем с покупкой!')
+                else:
+                    print('Авто не найдено в базе')
+            case 3:  # продажа авто
+                mark = input('Введите модель продаваемого авто:\n')
+                num = input('Введите регистрационный номер авто:\n')
+                own = tuple(input('Введите через пробел бывших владельцев авто:\n').split())
+                marks.append(mark)
+                numbers.append(num)
+                owners.append(own)
+                registry = list(zip(marks, numbers, owners))
+                print('Сделка заключена, поздравляем!')
+            case 4:  # поиск авто
+                num = input('Введите номер для поиска:\n')
+                num_ind = find_by_number(num, registry)
+                if num_ind >= 0:
+                    print('Есть такое авто:')
+                    print_car(registry[num_ind])
+                else:
+                    print('Авто не найдено в базе')
             case _:
                 print('Повторите, я вас не понял')
     except:
